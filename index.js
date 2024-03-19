d3ce5f6a94ce192d336043a052873a4341215b0d
Emmanuel Joel Odhiambo <joel.emmanuel1968@gmail.com>
2024-03-19 17:54:12 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let greeting;
+    if (distance <= 400){
+      greeting = "This one is on me!"
+} else if(distance <2000){
+      greeting = "That will be twenty bucks."
+} else if(distance <2500){
+      greeting = "I will gladly take your thirty bucks."
+} else {
+      greeting = "No can do."
 }
+return greeting;
+}  
 
-function ternaryCheckCity(){
+function ternaryCheckCity(NYC){
   // Write your code here!
+  const city="NYC"
+  return city===NYC? "Ok, sounds good." : "No go.";
 }
+console.log(ternaryCheckCity("LA"))
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+   
+  switch (tip) {
+    case "generous":
+      return "Thank you so much."
+    case "not as generous":
+      return "Thank you."
+    default:
+      return "Bye.";    
+  }
+  console.log(switchOnCharmFromTip("generous"))
+  
 }
